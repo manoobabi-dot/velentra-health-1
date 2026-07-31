73422973dc156faddcc5143696f9dea7b5261488
manoobabi-dot <manoo.babi@gmail.com>
2026-07-31 19:34:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const navbar = document.getElementById("navbar");
 
     if (menuToggle && navbar) {
-        // Menu open/close
         menuToggle.addEventListener("click", (e) => {
             e.stopPropagation();
             menuToggle.classList.toggle("active");
             navbar.classList.toggle("active");
         });
 
-        // Link click par menu close
         document.querySelectorAll("#navbar a").forEach(link => {
             link.addEventListener("click", () => {
                 menuToggle.classList.remove("active");
@@ -18,7 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
 
-        // Bahar click par menu close
         document.addEventListener("click", (e) => {
             if (!navbar.contains(e.target) && !menuToggle.contains(e.target)) {
                 menuToggle.classList.remove("active");
@@ -33,11 +30,9 @@ document.addEventListener("DOMContentLoaded", () => {
             const details = this.nextElementSibling;
 
             details.classList.toggle("active");
-
             this.textContent = details.classList.contains("active")
                 ? "Hide Details"
                 : "View Details";
         });
     });
-
 });
